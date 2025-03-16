00558dd25c3d8301afa5c0cafcc6a621d2f79e87
GreenRoc114 <3310149631@qq.com>
2025-03-16 10:42:06 +0800

    Site updated: 2025-03-16 10:42:05

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/14/hello-world/"];function toRandomPost(){
+var posts=["2025/03/14/hello-world/","2025/03/16/test/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
